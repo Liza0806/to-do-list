db1ef3ac9a06b0198a42da3ee1dc935c2027e8d9
Liza <fildepiorsta@gmail.com>
2024-04-26 13:51:29 +0300

    add add and remove tests

--- src/state/tasks-reducer.ts
+++ src/state/tasks-reducer.ts
@@ -2,37 +2,54 @@ import { v1 } from "uuid";
 import { TaskStateType } from "../App";
 import { TaskType } from "../components/ToDoList/ToDoList";
 
-export type Action1 = {
-  type: "1";
-  payload: string;
+export type RemoveTaskActionType = {
+  type: "REMOVE-TASK";
+  taskId: string;
+  todoId: string;
 };
-export type Action2 = {
-  type: "2";
-  payload: string;
+export type AddTaskActionType = {
+  type: "ADD-TASK";
+  taskTitle: string;
+  todoId: string;
 };
-type ActionsType =
-  | Action1
-  | Action2
-  export const toDoListReducer = (
-    state: Array<TaskStateType>,
-    action: ActionsType
-  ): Array<TaskType> => {
-    switch (action.type) {
-      case "1":
-        return '1'
-  
-      case "2":
-        return '2'
-  
-      
-      default:
-        throw new Error("dont understund u");
+type ActionsType = RemoveTaskActionType | AddTaskActionType;
+export const tasksReducer = (
+  state: TaskStateType,
+  action: ActionsType
+): TaskStateType => {
+  switch (action.type) {
+    case "REMOVE-TASK": {
+      let stateCopy = { ...state };
+      let tasks = state[action.todoId];
+      const filteredTask = tasks.filter((t: any) => t.id !== action.taskId);
+      stateCopy[action.todoId] = filteredTask;
+
+      return stateCopy;
     }
-  };
-  export const Action1AC = (id: string): Action1 => {
-    return { type: "1", payload: id };
-  };
-  
-  export const Action2AC = (title: string): Action2 => {
-    return { type: "2", payload: title };
-  };
+
+    case "ADD-TASK": {
+      let stateCopy = { ...state };
+      let tasks = state[action.todoId];
+      const taskItem = { id: v1(), title: action.taskTitle, isDone: false };
+      tasks = [taskItem, ...tasks];
+      stateCopy[action.todoId] = tasks;
+      return stateCopy;
+    }
+
+    default:
+      throw new Error("dont understund u");
+  }
+};
+export const removeTaskAC = (
+  taskId: string,
+  todoId: string
+): RemoveTaskActionType => {
+  return { type: "REMOVE-TASK", taskId: taskId, todoId: todoId };
+};
+
+export const addTaskAC = (
+  taskTitle: string,
+  todoId: string
+): AddTaskActionType => {
+  return { type: "ADD-TASK", taskTitle: taskTitle, todoId: todoId };
+};
